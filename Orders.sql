7a39382550f9c882eb5cb130c0593f58e0fda7a3
Iceflame07 <Chibizz007@gmail.com>
2025-01-15 10:17:47 +0100

    <Chapter 1>

---
+++ Orders.sql
@@ -0,0 +1,7 @@
+use assignment_db;
+CREATE TABLE orders(
+OrdersID INT PRIMARY KEY,
+OrdersNumber INT,
+OrdersAddress VARCHAR(200),
+OrdersTime INT
+);
---
+++ Product.sql
@@ -0,0 +1,8 @@
+use assignment_db;
+
+CREATE TABLE product(
+ProductID INT PRIMARY KEY,
+ProductName VARCHAR(50),
+ProductType VARCHAR(10),
+Product_Uni INT
+);
